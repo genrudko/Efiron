cb159d9db28274235056af8c54721885b1b687c2
genrudko <54811999+genrudko@users.noreply.github.com>
2026-07-27 22:46:07 +0300

    Use shared channel presentation in EPG

--- src/Efiron.App/Epg/EpgChannelListItem.cs
+++ src/Efiron.App/Epg/EpgChannelListItem.cs
@@ -1,10 +1,32 @@
+using Efiron.Core.Channels;
 using Efiron.Core.Playlists;
 
 namespace Efiron.App.Epg;
 
-internal sealed record EpgChannelListItem(
-    PlaylistChannel Channel,
-    string XmlTvChannelId)
+internal sealed class EpgChannelListItem
 {
-    public string Name => Channel.Name;
+    public EpgChannelListItem(PlaylistChannel channel, string xmlTvChannelId)
+    {
+        Channel = channel;
+        XmlTvChannelId = xmlTvChannelId;
+        Name = channel.Name;
+    }
+
+    public EpgChannelListItem(ChannelPresentation presentation, string xmlTvChannelId)
+    {
+        ArgumentNullException.ThrowIfNull(presentation);
+
+        Presentation = presentation;
+        Channel = presentation.EffectiveChannel;
+        XmlTvChannelId = xmlTvChannelId;
+        Name = presentation.NumberedName;
+    }
+
+    public ChannelPresentation? Presentation { get; }
+
+    public PlaylistChannel Channel { get; }
+
+    public string XmlTvChannelId { get; }
+
+    public string Name { get; }
 }
