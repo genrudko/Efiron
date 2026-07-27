6915ac6bc6a8751a333daf23cf019cde289a756c
genrudko <54811999+genrudko@users.noreply.github.com>
2026-07-28 00:14:59 +0300

    Add appearance accent model

---
+++ src/Efiron.Core/Appearance/AppAccentMode.cs
@@ -0,0 +1,13 @@
+namespace Efiron.Core.Appearance;
+
+public enum AppAccentMode
+{
+    Windows = 0,
+    Blue = 1,
+    Purple = 2,
+    Pink = 3,
+    Orange = 4,
+    Green = 5,
+    Teal = 6,
+    Custom = 7,
+}
