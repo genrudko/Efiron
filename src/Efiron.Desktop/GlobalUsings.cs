237cbc19615a3e5776d558d1da5035901d35a514
genrudko <54811999+genrudko@users.noreply.github.com>
2026-07-30 18:41:25 +0300

    REWRITE-001 disambiguate dispatcher priority

--- src/Efiron.Desktop/GlobalUsings.cs
+++ src/Efiron.Desktop/GlobalUsings.cs
@@ -1,2 +1,3 @@
 global using Efiron.Desktop.Presentation;
 global using DispatcherQueueTimer = Microsoft.UI.Dispatching.DispatcherQueueTimer;
+global using DispatcherQueuePriority = Microsoft.UI.Dispatching.DispatcherQueuePriority;
